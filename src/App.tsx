d9d97ce78516694e8fa8a4a15174138a158cd2fe
Pedro Henrique Durand Reis <webpedrod@gmail.com>
2022-07-18 14:21:32 -0300

    feat: dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import { Box, ChakraProvider, theme } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { AuthContextProvider } from "./context/authContext";
 import { Router } from "./router/router";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
+    <Box position="fixed" left="0" top="0" right="0" textAlign="right">
+      <ColorModeSwitcher justifySelf="flex-end" />
+    </Box>
     <BrowserRouter>
       <AuthContextProvider>
         <Router />
